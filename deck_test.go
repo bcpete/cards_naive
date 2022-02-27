a5cdb5f6b8f46de8346bf1d6e0ea9a9f4ab8620f
hometownzero <bradycpeters@gmail.com>
2022-02-27 13:16:16 -0600

    playing with go testing

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,35 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades but got %v", d[0])
+	}
+
+	if d[51] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs but got %v", d[0])
+	}
+}
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52 but got %v", len(loadedDeck))
+	}
+
+	if loadedDeck[0] != deck[0] {
+		t.Errorf("Expected first card to be %v, but got %v", deck[0], loadedDeck[0])
+	}
+
+	if loadedDeck[51] != deck[51] {
+		t.Errorf("Expected last card to be %v, but got %v", deck[51], loadedDeck[51])
+	}
+
+	os.Remove("_decktesting")
+
 }
